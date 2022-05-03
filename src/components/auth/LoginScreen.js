24c3627f8fdb8f69d80ebbe504947cdb91f2eded
Jimmy Rozo <jimmyrozo29@gmail.com>
2022-05-03 13:51:51 -0500

    Dispatch and thunk middleware configuration

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,23 +1,46 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { startLoginEmailPassword } from '../../actions/auth'
+import { useForm } from '../../hooks/useForm'
 
 export const LoginScreen = () => {
+
+  const dispatch = useDispatch();
+
+  const [ formValues, handleInputChange ] = useForm({
+    email: 'nando@gmail.com',
+    password: '123456'
+  })
+
+  const { email, password } = formValues
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch( startLoginEmailPassword( email, password ) )
+
+  }
+
   return (
     <>
       <h3 className='auth__title'>Login</h3>
-      <form>
+      <form onSubmit={ handleLogin } >
         <input
           type="text"
           placeholder='Email'
           name='email'
           className='auth__input'
           autoComplete='off'
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           type="password"
           placeholder='Password'
           name='password'
           className='auth__input'
+          value={password}
+          onChange={handleInputChange}
         />
         <button className='btn btn-primary btn-block' type='submit'>Login</button>
 
