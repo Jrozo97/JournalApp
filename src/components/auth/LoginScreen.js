eabf057b3388b883fb1316468acad42a32f3af95
Jimmy Rozo <jimmyrozo29@gmail.com>
2022-05-03 17:27:36 -0500

    Login with email and sign-in with google, route protection, error messages, login persistence

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { startLoginEmailPassword } from '../../actions/auth'
+import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth'
 import { useForm } from '../../hooks/useForm'
 
 export const LoginScreen = () => {
 
   const dispatch = useDispatch();
+  const { isLoading } = useSelector(state => state.ui)
 
-  const [ formValues, handleInputChange ] = useForm({
+  const [formValues, handleInputChange] = useForm({
     email: 'nando@gmail.com',
     password: '123456'
   })
@@ -17,14 +18,18 @@ export const LoginScreen = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    dispatch( startLoginEmailPassword( email, password ) )
+    dispatch(startLoginEmailPassword(email, password))
+  }
 
+  const handleGoogleLogin = () => {
+    dispatch(startGoogleLogin());
   }
 
   return (
     <>
       <h3 className='auth__title'>Login</h3>
-      <form onSubmit={ handleLogin } >
+      <form onSubmit={handleLogin} >
+        
         <input
           type="text"
           placeholder='Email'
@@ -34,6 +39,7 @@ export const LoginScreen = () => {
           value={email}
           onChange={handleInputChange}
         />
+
         <input
           type="password"
           placeholder='Password'
@@ -42,13 +48,21 @@ export const LoginScreen = () => {
           value={password}
           onChange={handleInputChange}
         />
-        <button className='btn btn-primary btn-block' type='submit'>Login</button>
+
+        <button
+          className='btn btn-primary btn-block'
+          type='submit'
+          disabled={isLoading}
+        >
+          Login
+        </button>
 
 
         <div className='auth__social-networks'>
           <p>Login with social networks</p>
           <div
             className="google-btn"
+            onClick={handleGoogleLogin}
           >
             <div className="google-icon-wrapper">
               <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,17 +1,73 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import validator from 'validator'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { useForm } from '../../hooks/useForm'
+import { removeError, setError } from '../../actions/ui'
+import { startRegisterEmailPassword } from '../../actions/auth'
 
 export const RegisterScreen = () => {
+
+  const dispatch = useDispatch();
+
+  const { msgError } = useSelector(state => state.ui);
+
+  const [formValues, handleInputChange] = useForm({
+    name: 'Henando',
+    email: 'nando@gmail.com',
+    password: '123456',
+    confirmPassword: '123456',
+  })
+
+  const { name, email, password, confirmPassword } = formValues
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    if (isFormValid()) {
+      dispatch( startRegisterEmailPassword(email, password, name) )
+    }
+  }
+
+  const isFormValid = () => {
+
+    if (name.trim().length === 0) {
+      dispatch(setError('Name is required'));
+      return false;
+    } else if (!validator.isEmail(email)) {
+      dispatch(setError('Email is not valid'));
+      return false;
+    } else if (password !== confirmPassword || password.length < 5) {
+      dispatch(setError('Password should be at least 6 characters and match each other'));
+      return false;
+    }
+
+    dispatch(removeError());
+    return true;
+
+  }
+
   return (
     <>
       <h3 className='auth__title'>Register</h3>
-      <form>
-      <input
+      <form onSubmit={handleRegister}>
+
+        {
+          msgError && (
+            <div className='auth__alert-error'>
+              { msgError }
+            </div>
+          )
+        }
+
+        <input
           type="text"
           placeholder='Name'
           name='name'
           className='auth__input'
           autoComplete='off'
+          value={name}
+          onChange={handleInputChange}
         />
         <input
           type="text"
@@ -19,23 +75,29 @@ export const RegisterScreen = () => {
           name='email'
           className='auth__input'
           autoComplete='off'
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           type="password"
           placeholder='Password'
           name='password'
           className='auth__input'
+          value={password}
+          onChange={handleInputChange}
         />
         <input
           type="password"
           placeholder='Confirm Password'
           name='confirmPassword'
           className='auth__input'
+          value={confirmPassword}
+          onChange={handleInputChange}
         />
         <button className='btn btn-primary btn-block mb-5' type='submit'>Register</button>
 
 
-        <Link to="/auth/register" className='link'>
+        <Link to="/auth/login" className='link'>
           Already registered?
         </Link>
 
